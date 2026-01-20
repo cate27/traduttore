7175ce304318d2dbc38c9f15adf2b25b9aee3e55
cate27 <86352345+cate27@users.noreply.github.com>
2026-01-20 13:40:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const languageSelect = document.getElementById('language-select');
+const languageSelectFrom = document.getElementById('languageFrom');
+const languageSelectTo = document.getElementById('languageTo');
+const textIn = document.querySelector('.textIn');
+const fromText = document.querySelector('.from-text');
+const toText = document.querySelector('.to-text');
 const textInput = document.querySelector('.text-input');
-const translationText = document.querySelector('.translation-text');
-const translationFlag = document.querySelector('.translation-flag');
+const translateFlagFrom = document.querySelector('.flag-from');
+const translateFlagTo = document.querySelector('.flag-to');
+const translationTextFrom = document.querySelector('.text-from');
+const translationsTextTo = document.querySelector('.text-to');
 const resetButton = document.querySelector('.reset-button');
-const favoriteButton = document.querySelector('.favorites');
+const favoriteButtonFrom = document.querySelector('.favorites-from');
+const favoriteButtonTo = document.querySelector('.favorites-to');
 const randomButton = document.querySelector('.random-button');
-const lang = 'it';
+const swapButton = document.querySelector('.fa-arrow-right-arrow-left');
+const removeSelectedButton = document.getElementById('remove-selected-button');
 let originBackground = '';
 
 //Creo una chiave per il local storage
@@ -20,11 +28,12 @@ function updateFavoriteList(translations) {
   translateFavorite.innerHTML = '';
   translations.forEach(function(translateTexts) {
     const li = document.createElement('li');
-    li.textContent = translateTexts;
+    li.textContent = `${translateTexts.fromText} → ${translateTexts.toText}`;
     translateFavorite.appendChild(li);
   });
 }
 
+//Seleziono l'elemento UL delle traduzioni preferite
 const translateFavorite = document.querySelector('.translate-favorites');
 //evento selezionare le traduzioni preferite
 translateFavorite.addEventListener('click', function(event) {
@@ -43,49 +52,91 @@ function loadTranslationsFromLocalStorage() {
 }
 
 //Funzione per salvare la traduzione preferita
-function saveTranslation() {
-  const translation = document.querySelector('.translation-text').innerText;
-  //Verifico se il testo è "Traduzione"
-  if (translation.trim() == "Traduzione") {
-    //Esco dalla funzione
+function saveTranslation(type) {
+  const translateFrom = translationTextFrom.innerText;
+  const translateTo = translationsTextTo.innerText;
+
+  // Se non c'è traduzione, esci
+  if (translateFrom.trim() === "Traduzione" || translateTo.trim() === "Traduzione") {
     return;
   }
-  //Aggiungo la traduzione preferita all'array
-  saveTranslations.push(translation);
-  //Salvo l'array nel local storage
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(saveTranslations));
-  //Aggiorno la lista delle traduzioni preferite visualizzate sulla pagina
-  updateFavoriteList(saveTranslations);
+
+  const fromLang = languageSelectFrom.value;
+  const toLang = languageSelectTo.value;
+
+  const favorite = {
+    fromText: translateFrom,
+    toText: translateTo,
+    fromLang,
+    toLang,
+    type // "from" o "to" se vuoi distinguere
+  };
+
+  // Evita duplicati
+  const exists = saveTranslations.some(item =>
+    item.fromText === favorite.fromText &&
+    item.toText === favorite.toText &&
+    item.fromLang === favorite.fromLang &&
+    item.toLang === favorite.toLang
+  );
+
+  if(exists) {
+    translationTextFrom.value = 'Traduzione già salvata';
+    return;
+  }
+
+  if (!exists) {
+    saveTranslations.push(favorite);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(saveTranslations));
+    updateFavoriteList(saveTranslations);
+  }
 }
 
-//Funzione per gestire il click sull'icona e salvare le traduzioni preferite
-favoriteButton.addEventListener('click', function() {
-  saveTranslation();
+favoriteButtonFrom.addEventListener('click', () => {
+  saveTranslation("from");
 });
+
+favoriteButtonTo.addEventListener('click', () => {
+  saveTranslation("to");
+});
+
 
 //Eseguo il caricamento delle traduzioni salvate quando la pagina viene caricata
 window.addEventListener('load', function() {
   loadTranslationsFromLocalStorage();
 });
 
-//Funzione per inserire la bandiera come sfondo 
-function changeBackground(lang) {
-  const translation = document.querySelector('.translation');
-  //rimuovo le classi relative alle bandiere
-  translation.classList.remove("ar-bg", "bg-bg", "cs-bg", "hr-bg", "da-bg", "et-bg", "fr-bg", "fi-bg", "ja-bg", "el-bg", "en-bg", "ga-bg", "lt-bg", "mt-bg", "no-bg", "nl-bg", "pl-bg", "pt-bg", "ro-bg", "ru-bg", "sk-bg", "sl-bg", "es-bg", "sv-bg", "de-bg", "tr-bg", "hu-bg");
-  //aggiungo le classi relative alle bandiere
-  translation.classList.add(lang + "-bg");
+//Funzione per cambiare lo sfondo delle bandiere
+function changeBackground() {
+  const fromLang = languageSelectFrom.value;
+  const toLang = languageSelectTo.value;
+
+  if (fromLang && countries[fromLang]) {
+    translateFlagFrom.style.backgroundImage =
+      `url(images/flags/${countries[fromLang].flag}.svg)`;
+  }
+
+  if (toLang && countries[toLang]) {
+    translateFlagTo.style.backgroundImage =
+      `url('images/flags/${countries[toLang].flag}.svg')`;
+  }
 }
 
-function reset(lang) {
-  textInput.value = '';
-  translationText.innerText = 'Traduzione';
-  translationFlag.innerText = '';
-  // Rimuovo lo sfondo
-  const translation = document.querySelector('.translation');
-  translation.style.backgroundImage = "none";
+languageSelectFrom.addEventListener('change', changeBackground);
+languageSelectTo.addEventListener('change', changeBackground);
 
-  changeBackground(lang);
+
+//Funzione per resettare i campi
+function reset() {
+  fromText.value = '';
+  toText.value = '';
+  languageSelectFrom.value = '';
+  languageSelectTo.value = '';
+  // Rimuovo lo sfondo
+  translateFlagFrom.style.backgroundImage = "none";
+  translateFlagTo.style.backgroundImage = "none";
+  translationTextFrom.innerText = 'Traduzione';
+  translationsTextTo.innerText = 'Traduzione';
 }
 
 //funzione per ripristinare lo sfondo originale
@@ -94,51 +145,120 @@ function restoreBackground() {
   translation.style.backgroundImage = originBackground;
 }
 
-async function translate(text, lang, flag) {
-  const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=it|${lang}`;
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  const result = jsonData.responseData.translatedText;
-  console.log(result);
+//Funzione per cambiare la traduzione
+swapButton.addEventListener('click', function() {
+  const currentText = languageSelectFrom.value;
+  const currentTranslation = languageSelectTo.value;
 
-  translationText.innerText = result;
-  translationFlag.innerText = flag;
+  languageSelectFrom.value = currentTranslation;
+  languageSelectTo.value = currentText;
 
+  // Scambio il testo tra i due campi
+  const tempText = fromText.value;
+  fromText.value = toText.value;
+  toText.value = tempText;
+
+  //Scambio il testo delle traduzioni
+  const tempTranslationText = translationTextFrom.innerText;
+  translationTextFrom.innerText = translationsTextTo.innerText;
+  translationsTextTo.innerText = tempTranslationText;
+
+  //Aggiorno lo sfondo delle bandiere
+  changeBackground();
+
+  // Ripristino lo sfondo originale
   restoreBackground();
+});
+
+//Popolo le select con le lingue disponibili
+function populateLanguages() {
+  Object.entries(countries)
+    .sort((a, b) => a[1].name.localeCompare(b[1].name, 'it'))
+    .forEach(([code, data]) => {
+      const optionFrom = document.createElement("option");
+      optionFrom.value = code;
+      optionFrom.textContent = data.name;
+
+      const optionTo = optionFrom.cloneNode(true);
+      //optionTo.textContent = data.name;
+      languageSelectFrom.appendChild(optionFrom);
+      languageSelectTo.appendChild(optionTo);
+  });
 }
 
-languageSelect.addEventListener('change', function() {
-  const selectedOption = languageSelect.options[languageSelect.selectedIndex];
+populateLanguages();
 
-    // recupero il testo dal campo di input e rimuovo eventuali spazi extra
-    // all'inizio e alla fine della stringa inserita con il metodo .trim()
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
-    const text = textInput.value.trim();
+function canTranslate() {
+  return (
+    fromText.value.trim() !== '' &&
+    languageSelectFrom.value !== '' &&
+    languageSelectTo.value !== ''
+  );
+}
 
-    // recupero il codice lingua dal data-attribute del pulsante
-    const lang = selectedOption.dataset.lang;
-    // recupero la bandierina dalla testo del pulsante
-    const flag = selectedOption.innerText;
+//Funzione per tradurre il testo
+async function translate() {
+  if (!canTranslate()) return;
+  const text = fromText.value.trim();
+  if (!text) return;
 
-    //recupero lo sfondo
-    const backgroundLang = this.getAttribute("data-lange");
-    changeBackground(lang);
+  const translateFrom = languageSelectFrom.value;
+  const translateTo = languageSelectTo.value;
 
-    // se il campo di input ha effettvamente del testo
-    // invoco la funzione e faccio partire la chiamata alle API
-    if(text.length > 0) {
-      translate(text, lang, flag);
-    }
-});
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=${translateFrom}|${translateTo}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  toText.value = data.responseData.translatedText;
+
+  translationTextFrom.innerText = fromText.value;
+  translationsTextTo.innerText = data.responseData.translatedText;
+
+  changeBackground();
+}
+
+fromText.addEventListener('keyup', translate);
+
+languageSelectFrom.addEventListener('change', translate);
+languageSelectTo.addEventListener('change', translate);
 
 
 //funzione per inserire frasi random
 async function randomString() {
-  const response = await fetch('https://random-word-api.herokuapp.com/word?lang=it');
-  const work = await response.json();
-  console.log(work);
-  textInput.value = work;
+  const fromLang = languageSelectFrom.value;
+
+  // controllo lingua selezionata
+  if (!fromLang) {
+    toText.value = 'Seleziona prima la lingua di partenza';
+    return;
+  }
+
+  let randomWord = '';
+
+  //dizionario locale
+  if (randomDictionary[fromLang]) {
+    const words = randomDictionary[fromLang];
+    randomWord = words[Math.floor(Math.random() * words.length)];
+  }
+  //fallback API (inglese)
+  else {
+    try {
+      const response = await fetch('https://random-word-api.herokuapp.com/word');
+      const [word] = await response.json();
+      randomWord = word;
+    } catch (error) {
+      toText.value = 'Errore nel caricamento parola casuale';
+      return;
+    }
+  }
+
+  fromText.value = randomWord;
+
+  // traduce subito
+  translate();
 }
+
 
 randomButton.addEventListener('click', function() {
   randomString();
@@ -149,23 +269,23 @@ resetButton.addEventListener('click', reset);
 
 
 //funzione per rimuovere le parole preferite
-const removeSelectedButton = document.querySelector('#remove-selected-button');
 removeSelectedButton.addEventListener('click', function() {
   const selectedItems = document.querySelectorAll('.translate-favorites .selected');
 
-  //rimuovo le traduzioni selezionate sia dal DOM che dall'array
   selectedItems.forEach(function(item) {
     const translationText = item.textContent;
-    //Rimuovo la traduzione dall'array saveTranslations
+
     saveTranslations = saveTranslations.filter(function(translation) {
-      return translation !== translationText;
+      // ricreo la stringa uguale a quella in lista
+      const text = `${translation.fromText} → ${translation.toText}`;
+      return text !== translationText;
     });
-    //rimuovo l'elemento dal DOM
+
     item.remove();
   });
 
-  //Aggiorno il localStorage con l'array aggiornato
   localStorage.setItem(STORAGE_KEY, JSON.stringify(saveTranslations));
-  //aggiorno la lista delle traduzioni visualizzate
   updateFavoriteList(saveTranslations);
 });
+
+
